5dee3e68491df6b4dc9ed16455d3e8d62e88c990
pedroaal <pedroaal04@gmail.com>
2024-03-12 11:40:04 -0500

    added translation

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,29 +1,35 @@
 import { type Component } from 'solid-js'
 
+import { ROUTES } from '../constants/router'
+import { useTranslateStore } from '../context/locale.context'
+
 import LanguageSelector from '../components/LanguageSelector'
 import ThemeToggler from '../components/ThemeToggler'
-import { ROUTES } from '../constants/router'
 
-const Header: Component = () => (
-  <header class="navbar bg-primary-100">
-    <div class="flex-1">
-      <a class="btn btn-ghost text-xl" href={ROUTES.HOME}>
-        Luxury
-      </a>
-    </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal px-1">
-        <li>
-          <a href={ROUTES.ABOUT}>About us</a>
-        </li>
-      </ul>
-      <div class="flex gap-2">
-        <LanguageSelector />
-        <ThemeToggler />
-        <a class="btn btn-primary">Button</a>
+const Header: Component = () => {
+  const { t } = useTranslateStore()
+
+  return (
+    <header class="navbar bg-primary-100">
+      <div class="flex-1">
+        <a class="btn btn-ghost text-xl" href={ROUTES.HOME}>
+          {t('navigation.home')}
+        </a>
       </div>
-    </div>
-  </header>
-)
+      <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+          <li>
+            <a href={ROUTES.ABOUT}>{t('navigation.about')}</a>
+          </li>
+        </ul>
+        <div class="flex gap-2">
+          <LanguageSelector />
+          <ThemeToggler />
+          <a class="btn btn-primary">{t('navigation.action')}</a>
+        </div>
+      </div>
+    </header>
+  )
+}
 
 export default Header
